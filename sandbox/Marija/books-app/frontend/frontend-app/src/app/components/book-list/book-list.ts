0b2727059b3765c805dd1641bfb6a15bdc7544dd
Marija Miljkovic <marija.miljkovic19@gmail.com>
2026-03-16 12:37:40 +0100

    Dodavanje i brisanje (mini aplikacija)
    Dodate funkcije dodavanja i brisanja procitanih knjiga direktno sa stranice

--- sandbox/Marija/books-app/frontend/frontend-app/src/app/components/book-list/book-list.ts
+++ sandbox/Marija/books-app/frontend/frontend-app/src/app/components/book-list/book-list.ts
@@ -1,18 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { BookService } from '../../services/book';
 import { Book } from '../../models/book';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
   templateUrl: './book-list.html',
-  imports: [CommonModule]
+  imports: [CommonModule, FormsModule]
 })
 export class BookListComponent implements OnInit {
   books: Book[] = [];
 
-  constructor(private bookService: BookService) {}
+  newBook : Book = {
+    id: 0, 
+    title: '',
+    author: '',
+    year: new Date().getFullYear()
+  };
+
+  constructor(
+    private bookService: BookService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.loadBooks();
@@ -21,12 +32,32 @@ export class BookListComponent implements OnInit {
   loadBooks() {
     this.bookService.getBooks().subscribe((data: any) => {
       this.books = data.$values ?? data; // obrađuje .NET List
+      this.cd.detectChanges();
     });
+  }
+
+  addBook() {
+    
+    if (!this.newBook.title || !this.newBook.author) {
+    return;
+  }
+
+    this.bookService.addBook(this.newBook).subscribe(() => {
+      this.loadBooks();
+      this.newBook = { 
+        id: 0, 
+        title: '', 
+        author: '', 
+        year: new Date().getFullYear() };
+    });
+
+    this.cd.detectChanges();
   }
 
   deleteBook(id: number) {
     this.bookService.deleteBook(id).subscribe(() => {
-      this.loadBooks(); // osveži listu
+      this.loadBooks();
+      this.cd.detectChanges();
     });
   }
 }
