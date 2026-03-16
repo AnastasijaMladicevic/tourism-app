d528c57a1112f70629cec917efe877c1d4eac1f3
MihailoObradovic792022 <miki.obradovic24@gmail.com>
2026-03-16 19:50:21 +0100

    napravljen solution za sandbox backend i dodat Kategorije.cs fajl kao model tu

---
+++ sandbox/Mihajlo/Backend/Net_Bekend_ucenje/Models/Kategorije.cs
@@ -0,0 +1,12 @@
+﻿namespace Net_Bekend_ucenje.Models
+{
+    public class Kategorije
+    {
+        public Guid Id { get; set; }
+        public string Naziv { get; set; } = string.Empty;
+        public string? Ikonica { get; set; }
+        public DateTime Kreiran { get; set; }
+
+        public ICollection<Destinacija> Destinacije { get; set; } = new List<Destinacija>();
+    }
+}
