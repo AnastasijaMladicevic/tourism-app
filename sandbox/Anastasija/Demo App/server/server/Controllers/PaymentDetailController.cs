1946b292bedc88e0d905671b354170b4a03d3ac6
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-17 00:38:34 +0100

    ispravljene greske sredjen gitignore

--- sandbox/Anastasija/Demo App/server/server/Controllers/PaymentDetailController.cs
+++ sandbox/Anastasija/Demo App/server/server/Controllers/PaymentDetailController.cs
@@ -21,14 +21,12 @@ namespace server.Controllers
             _context = context;
         }
 
-        // GET: api/PaymentDetail
         [HttpGet]
         public async Task<ActionResult<IEnumerable<PaymentDetail>>> GetPaymentDetails()
         {
             return await _context.PaymentDetails.ToListAsync();
         }
 
-        // GET: api/PaymentDetail/5
         [HttpGet("{id}")]
         public async Task<ActionResult<PaymentDetail>> GetPaymentDetail(int id)
         {
@@ -42,8 +40,6 @@ namespace server.Controllers
             return paymentDetail;
         }
 
-        // PUT: api/PaymentDetail/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutPaymentDetail(int id, PaymentDetail paymentDetail)
         {
@@ -73,8 +69,6 @@ namespace server.Controllers
             return Ok(await _context.PaymentDetails.ToListAsync());
         }
 
-        // POST: api/PaymentDetail
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<PaymentDetail>> PostPaymentDetail(PaymentDetail paymentDetail)
         {
@@ -84,7 +78,6 @@ namespace server.Controllers
             return Ok(await _context.PaymentDetails.ToListAsync());
         }
 
-        // DELETE: api/PaymentDetail/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeletePaymentDetail(int id)
         {
