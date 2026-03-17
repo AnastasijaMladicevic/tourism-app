aa94779015735e799946aa521080ad1022c17cea
Ivana Bacanin <26-2021@pmf.kg.ac.rs>
2026-03-17 19:49:36 +0100

    zavrsen demo projekat

--- sandbox/Ivana/demo-app-ivana/src/app/app.ts
+++ sandbox/Ivana/demo-app-ivana/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { StudentsComponent } from './components/students/students.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  standalone: true,
+  imports: [HttpClientModule, StudentsComponent],
+  template: `<app-students></app-students>`,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('demo-app-ivana');
 }
