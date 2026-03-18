5b025a1d86c32060f6fc697aa7046abbfe63b428
Marija Miljkovic <marija.miljkovic19@gmail.com>
2026-03-18 23:53:07 +0100

    Ispravka demo aplikacije
    Dodate rute u frontend

--- sandbox/Marija/books-app/frontend/frontend-app/src/app/app.routes.ts
+++ sandbox/Marija/books-app/frontend/frontend-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list';
+import { BookDetailsComponent } from './components/book-details/book-details';
+import { AddBookComponent } from './components/add-book/add-book';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+
+  { path: 'books', component: BookListComponent },
+  { path: 'books/:id', component: BookDetailsComponent },
+
+  { path: 'add-book', component: AddBookComponent },
+
+  { path: '**', redirectTo: 'books' }
+];
