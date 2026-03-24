155b0bf74e58cdc4cc77a2a3a8c098250b21ccbf
Marija Miljkovic <marija.miljkovic19@gmail.com>
2026-03-24 15:42:02 +0100

    Dodate klase, male izmene
    Dodate klase KorisnikAktivnost, KorisnikLog i Slika, sitne ispravke u sql skripti i klasi TipDogadjaja

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/KorisnikAktivnost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models;
+
+public class KorisnikAktivnost
+{
+    public int Id { get; set; }
+
+    public int IdKorisnika { get; set; }
+    public int IdAktivnosti { get; set; }
+
+    public DateTime Kreirano { get; set; }
+
+    // Navigacije
+    public Korisnik Korisnik { get; set; } = null!;
+    public Aktivnost Aktivnost { get; set; } = null!;
+}
