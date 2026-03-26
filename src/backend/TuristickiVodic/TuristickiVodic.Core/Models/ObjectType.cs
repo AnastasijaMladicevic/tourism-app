f755596bd142593c2b22d5c42c311a56f271d0ec
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-26 22:54:33 +0100

    dodata klasa ObjectType.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/ObjectType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class ObjectType
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(80)]
+        public string Name { get; set; }
+        // "Hotel", "Restoran", "Pumpa"
+
+        public ICollection<TouristObject> Objects { get; set; }
+    }
+}
