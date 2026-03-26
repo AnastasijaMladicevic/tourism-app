6be97a029f99b7b6bf2d1e557c20ea697444e287
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-26 22:46:06 +0100

    dodata klasa ActivityType.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/ActivityType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class ActivityType
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(80)]
+        public string Name { get; set; }
+
+        public ICollection<Activity> Activities { get; set; }
+    }
+}
