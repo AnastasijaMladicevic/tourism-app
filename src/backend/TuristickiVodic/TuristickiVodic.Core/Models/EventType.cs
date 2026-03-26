3702aa35f94bac650c05a2e0db51d088729f9830
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-26 22:48:29 +0100

    dodata klasa EventType.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/EventType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class EventType
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(80)]
+        public string Name { get; set; }
+
+        public ICollection<Event> Events { get; set; }
+    }
+
+}
