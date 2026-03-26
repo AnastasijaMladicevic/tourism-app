24ff1ee0f46d7d01700223d240939cd233b4bd39
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-26 22:56:30 +0100

    dodata klasa LocationType.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/LocationType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class LocationType
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+        // "Grad", "Opstina", "Region"
+
+        public ICollection<Location> Locations { get; set; }
+    }
+}
