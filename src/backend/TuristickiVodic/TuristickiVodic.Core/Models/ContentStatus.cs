6fcba028a0becf457f925686bba749a1124851ea
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-26 22:46:43 +0100

    dodat enum ContentStatus.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/ContentStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models
+{
+    public enum ContentStatus
+    {
+        Pending = 0,
+        Approved = 1,
+        Rejected = 2
+    }
+
+}
