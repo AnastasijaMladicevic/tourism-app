2a3071caf8d81d8c4101ac4eea3b7a865ed1dd11
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-26 22:48:00 +0100

    dodata klasa DestinationType.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/DestinationType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class DestinationType
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+        // "Stari Grad", "Plaza", "Park"
+
+        public ICollection<Destination> Destinations { get; set; }
+    }
+}
