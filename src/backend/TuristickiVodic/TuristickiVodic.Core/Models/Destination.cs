f239028963af8d3fc2810e2ed27f1d88d36d1240
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-26 22:44:39 +0100

    dodata klasa Destination.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/Destination.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using NetTopologySuite.Geometries;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(150)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+        public Point? Geolocation { get; set; }
+
+        public ContentStatus Status { get; set; }
+            = ContentStatus.Pending;
+
+        public bool IsActive { get; set; } = true;
+
+        [Required]
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+
+        [Required]
+        public int DestinationTypeId { get; set; }
+        public DestinationType DestinationType { get; set; }
+
+        [Required]
+        public int CreatedByUserId { get; set; }
+
+        [ForeignKey("CreatedByUserId")]
+        public User CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Image> Images { get; set; }
+        public ICollection<Favorite> Favorites { get; set; }
+        public ICollection<TouristObject> Objects { get; set; }
+        public ICollection<Activity> Activities { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
