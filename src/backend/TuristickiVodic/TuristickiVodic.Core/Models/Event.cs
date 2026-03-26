c42cfb18726a9e6456a209f6a056251b39a69f34
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-26 22:44:06 +0100

    dodata klasa Event.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/Event.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using NetTopologySuite.Geometries;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(200)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+        public Point? Geolocation { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public decimal? Price { get; set; }
+        public int? MaxVisitors { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public ContentStatus Status { get; set; }
+            = ContentStatus.Pending;
+
+        [Required]
+        public int EventTypeId { get; set; }
+        public EventType EventType { get; set; }
+
+        [Required]
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+
+        public int? DestinationId { get; set; }
+        public Destination? Destination { get; set; }
+
+        public int? ObjectId { get; set; }
+        public TouristObject? Object { get; set; }
+
+        [Required]
+        public int CreatedByUserId { get; set; }
+
+        [ForeignKey("CreatedByUserId")]
+        public User CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Image> Images { get; set; }
+        public ICollection<Favorite> Favorites { get; set; }
+    }
+}
