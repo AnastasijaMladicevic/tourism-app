95500498d18d5602546576cba19637b7acf28b5c
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-27 19:32:51 +0100

    Ispravljen fajl RoleType.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/RoleType.cs
@@ -0,0 +1,10 @@
+﻿namespace TuristickiVodic.Core.Models
+{
+    public enum RoleType
+    {
+        Tourist = 1,      // običan turista
+        ContentCreator = 2, // može dodavati objekte i eventove
+        Manager = 3,       // upravlja destinacijom
+        Admin = 4          // sve može
+    }
+}
