c55e61d1393ddc050ef99bf972477269e80a483f
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-27 19:31:11 +0100

    Ispravljen fajl LogAction.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/LogAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models
+{
+    public enum LogAction
+    {
+        View,
+        Click,
+        Favourite,
+        Review,
+        Search,
+        Share
+    }
+}
