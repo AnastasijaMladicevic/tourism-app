63c8a5e9690402e66afe2b81f0285d5fc00cd148
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-27 19:32:37 +0100

    Ispravljen fajl Role.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public RoleType Name { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<User> Users { get; set; }
+    }
+}
