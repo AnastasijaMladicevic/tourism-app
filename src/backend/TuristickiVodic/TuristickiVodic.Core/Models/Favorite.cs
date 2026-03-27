6788969699a2f1e6682a0f7b84d4fa32e67fb37e
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-27 19:29:51 +0100

    Ispravljen fajl Favorite.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/Favorite.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int? ObjectId { get; set; }
+        public TouristObject? Object { get; set; }
+
+        public int? ActivityId { get; set; }
+        public Activity? Activity { get; set; }
+
+        public int? DestinationId { get; set; }
+        public Destination? Destination { get; set; }
+
+        public int? RouteId { get; set; }
+        public Route? Route { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
