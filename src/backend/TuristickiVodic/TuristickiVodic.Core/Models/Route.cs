5628022a9b0166d4a375cd6ccbc70958f23da184
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-27 19:33:04 +0100

    Ispravljen fajl Route.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/Route.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class Route
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(200)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [MaxLength(20)]
+        public string? Difficulty { get; set; }
+
+        public decimal? LengthKm { get; set; }
+
+        public int? CreatedByUserId { get; set; }
+        [ForeignKey("CreatedByUserId")]
+        public User? CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<RoutePoint> RoutePoints { get; set; }
+        public ICollection<Favorite> Favorites { get; set; }
+    }
+}
