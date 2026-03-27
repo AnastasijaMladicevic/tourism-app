d3c9291e8445a903e4730411c86bec6566479259
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-27 19:33:22 +0100

    Ispravljen fajl RoutePoint.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/RoutePoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using NetTopologySuite.Geometries;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class RoutePoint
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int RouteId { get; set; }
+        public Route Route { get; set; }
+
+        public short Order { get; set; }
+        public Point Geolocation { get; set; }
+
+        [MaxLength(150)]
+        public string? PointName { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
