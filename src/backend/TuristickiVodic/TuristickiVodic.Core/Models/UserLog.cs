8f310f12a7b7ef1a6670a5ae81ad64a15f456dbd
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-27 19:34:39 +0100

    Ispravljen fajl UserLog.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/Models/UserLog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TuristickiVodic.Core.Models
+{
+    public class UserLog
+    {
+        public long Id { get; set; }
+
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+
+        // Za anonimne korisnike
+        [MaxLength(100)]
+        public string? SessionId { get; set; }
+
+        public int? ObjectId { get; set; }
+        public TouristObject? Object { get; set; }
+
+        [Required]
+        public LogAction Action { get; set; }
+
+        public int? DurationSeconds { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
