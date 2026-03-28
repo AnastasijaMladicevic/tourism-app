bef682ea1e8336c3385dece7421516de957291a2
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-28 12:05:41 +0100

    Dodat ChangePasswordDto.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/ChangePasswordDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TuristickiVodic.Core.DTOs
+{
+    public class ChangePasswordDto
+    {
+        [Required]
+        public string CurrentPassword { get; set; }
+
+        [Required, MinLength(6)]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword")]
+        public string ConfirmPassword { get; set; }
+    }
+}
