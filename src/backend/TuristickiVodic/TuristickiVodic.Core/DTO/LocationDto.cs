924d08fb8979f4ebe91426ec5d2f92df7ed80fd8
Marija Miljkovic <marija.miljkovic19@gmail.com>
2026-03-28 18:47:54 +0100

    Dodate klase za Location
    Dodate LocationDto, CreateLocationDto i UpdateLocationDto

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/LocationDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuristickiVodic.Core.DTO
+{
+    public class LocationDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public double? Latitude { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int DestinationId { get; set; }
+
+        public string DestinationName { get; set; } = string.Empty;
+
+        public int LocationTypeId { get; set; }
+
+        public string LocationTypeName { get; set; } = string.Empty;
+
+        public int? CreatedByUserId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
