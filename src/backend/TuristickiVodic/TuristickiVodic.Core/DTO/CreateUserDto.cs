43075355f214f23e5d490be6073bee37ef408e03
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-28 12:06:20 +0100

    Dodat CreateUserDto.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/CreateUserDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TuristickiVodic.Core.DTOs
+{
+    public class CreateUserDto
+    {
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required, EmailAddress, MaxLength(200)]
+        public string Email { get; set; }
+
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+
+        [MaxLength(20)]
+        public string? PhoneNumber { get; set; }
+
+        [MaxLength(100)]
+        public string? Country { get; set; }
+
+        [MaxLength(5)]
+        public string Language { get; set; } = "sr";
+    }
+}
