4a38b9a282fc1d1567f05018a0261ec30469fd46
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-28 12:04:36 +0100

    Dodat AuthResponseDto.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TuristickiVodic.Core.DTOs
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; }
+        public UserDto User { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
