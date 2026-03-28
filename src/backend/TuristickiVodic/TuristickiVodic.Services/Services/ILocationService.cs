1148c8b80eda1efb9d9699b660922bdc03f59cac
Marija Miljkovic <marija.miljkovic19@gmail.com>
2026-03-28 18:45:46 +0100

    Servis za lokaciju
    Dodati interfejs za servis za lokaciju i klasa koja ga implementira

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Services/Services/ILocationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TuristickiVodic.Core.DTO;
+
+namespace TuristickiVodic.Services.Services
+{
+    public interface ILocationService
+    {
+        Task<IEnumerable<LocationDto>> GetAllAsync();
+        Task<LocationDto?> GetByIdAsync(int id);
+        Task<LocationDto> CreateAsync(CreateLocationDto dto, int userId, string roleName);
+        Task<LocationDto?> UpdateAsync(int id, UpdateLocationDto dto, int userId, string roleName);
+        Task<bool> DeleteAsync(int id, int userId, string roleName);
+    }
+}
