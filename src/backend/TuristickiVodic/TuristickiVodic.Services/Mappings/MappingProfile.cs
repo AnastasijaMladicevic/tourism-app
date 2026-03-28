d77a4559d2a73710d77d6d241c9357fe6d015d3b
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-28 12:45:37 +0100

    dodat mappingprofile.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Services/Mappings/MappingProfile.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using TuristickiVodic.Core.DTOs;
+using TuristickiVodic.Core.Models;
+
+namespace TuristickiVodic.Services.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.Name.ToString()));
+
+            CreateMap<CreateUserDto, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.RoleId, opt => opt.Ignore())
+                .ForMember(dest => dest.Role, opt => opt.Ignore())
+                .ForMember(dest => dest.ManagedDestinationId, opt => opt.Ignore())
+                .ForMember(dest => dest.ManagedDestination, opt => opt.Ignore())
+                .ForMember(dest => dest.IsVerified, opt => opt.MapFrom(src => false))
+                .ForMember(dest => dest.VerificationToken, opt => opt.Ignore())
+                .ForMember(dest => dest.VerificationTokenExpiry, opt => opt.Ignore())
+                .ForMember(dest => dest.ResetToken, opt => opt.Ignore())
+                .ForMember(dest => dest.ResetTokenExpiry, opt => opt.Ignore())
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true))
+                .ForMember(dest => dest.IsBlacklisted, opt => opt.MapFrom(src => false))
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Reviews, opt => opt.Ignore())
+                .ForMember(dest => dest.Favorites, opt => opt.Ignore())
+                .ForMember(dest => dest.UserLogs, opt => opt.Ignore())
+                .ForMember(dest => dest.SentReports, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedObjects, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedEvents, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedDestinations, opt => opt.Ignore());
+
+            CreateMap<UpdateUserDto, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.RoleId, opt => opt.Ignore())
+                .ForMember(dest => dest.Role, opt => opt.Ignore())
+                .ForMember(dest => dest.ManagedDestinationId, opt => opt.Ignore())
+                .ForMember(dest => dest.ManagedDestination, opt => opt.Ignore())
+                .ForMember(dest => dest.IsVerified, opt => opt.Ignore())
+                .ForMember(dest => dest.VerificationToken, opt => opt.Ignore())
+                .ForMember(dest => dest.VerificationTokenExpiry, opt => opt.Ignore())
+                .ForMember(dest => dest.ResetToken, opt => opt.Ignore())
+                .ForMember(dest => dest.ResetTokenExpiry, opt => opt.Ignore())
+                .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+                .ForMember(dest => dest.IsBlacklisted, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Reviews, opt => opt.Ignore())
+                .ForMember(dest => dest.Favorites, opt => opt.Ignore())
+                .ForMember(dest => dest.UserLogs, opt => opt.Ignore())
+                .ForMember(dest => dest.SentReports, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedObjects, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedEvents, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedDestinations, opt => opt.Ignore())
+                .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+        }
+    }
+}
