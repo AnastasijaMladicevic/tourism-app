5556742452ab12296892341f3875638ad8f06e93
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-29 13:34:55 +0200

    Dodat DestinationDto.cs

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/DestinationDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TuristickiVodic.Core.DTOs
+{
+    public class DestinationDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public double? Latitude { get; set; }
+
+        public string Status { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; }
+
+        public int DestinationTypeId { get; set; }
+
+        public string DestinationTypeName { get; set; } = string.Empty;
+
+        public int CreatedByUserId { get; set; }
+
+        public int? ManagedByUserId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
