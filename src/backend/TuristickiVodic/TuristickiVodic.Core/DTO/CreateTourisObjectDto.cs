dc488ccabf2814df797df9cb52eca5a3a1694efc
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-30 02:54:02 +0200

    Dodat Dto za kreiranje turistickog objekta

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/CreateTourisObjectDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TuristickiVodic.Core.DTO
+{
+    public class CreateTouristObjectDto
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [MaxLength(300)]
+        public string? Address { get; set; }
+
+        [MaxLength(30)]
+        public string? PhoneNumber { get; set; }
+
+        [MaxLength(300)]
+        public string? Website { get; set; }
+
+        public string? WorkingHours { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public double? Latitude { get; set; }
+
+        [Required]
+        public int ObjectTypeId { get; set; }
+
+        [Required]
+        public int LocationId { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
