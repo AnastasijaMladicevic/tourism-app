43cb5317a5e548e2163596668e62fe010d6b811c
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-03-30 03:08:37 +0200

    dodat updatedto za objekat

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/UpdateTouristObjectDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TuristickiVodic.Core.DTO
+{
+    public class UpdateTouristObjectDto
+    {
+        [MaxLength(200)]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [MaxLength(300)]
+        public string? Address { get; set; }
+
+        [MaxLength(30)]
+        public string? PhoneNumber { get; set; }
+
+        [MaxLength(300)]
+        public string? Website { get; set; }
+
+        public string? WorkingHours { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public double? Latitude { get; set; }
+
+        public int? ObjectTypeId { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}
