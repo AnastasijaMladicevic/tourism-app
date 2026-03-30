bc23467fc062e93da388f74b370c6370648a4f77
Milan Jovanovic <60-2023@pmf.kg.ac.rs>
2026-03-30 23:33:14 +0200

    Uradjen Login dizajn i neke funkcionalnosti.

--- src/frontend/front-mobilna/src/app/app.routes.ts
+++ src/frontend/front-mobilna/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./feature/splash/splash').then(m => m.SplashComponent) },
+  // { path: '', loadComponent: () => import('./feature/splash/splash').then(m => m.SplashComponent) },
   { path: 'login', loadComponent: () => import('./feature/login/login').then(m => m.LoginComponent) },
   // { path: 'register', loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent) },
   // { path: 'forgot-password', loadComponent: () => import('./features/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent) },
