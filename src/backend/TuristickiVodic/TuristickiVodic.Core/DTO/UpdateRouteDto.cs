fb6e5060a2437a69f8be12a4066731a73569952f
Marija Miljkovic <marija.miljkovic19@gmail.com>
2026-03-31 11:23:19 +0200

    Dodat DTO za UpdateRoute

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/UpdateRouteDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TuristickiVodic.Core.DTO
+{
+    public class UpdateRouteDto
+    {
+        [MaxLength(200)]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [MaxLength(20)]
+        public string? Difficulty { get; set; }
+
+        public decimal? LengthKm { get; set; }
+
+        // Ako je prosleđeno, zamenjuje sve tačke rute
+        public List<CreateRoutePointDto>? RoutePoints { get; set; }
+    }
+}
