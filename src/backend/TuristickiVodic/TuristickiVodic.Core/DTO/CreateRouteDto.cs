47debe7e210d8eae4ba6f67ce190997f0f07088e
Marija Miljkovic <marija.miljkovic19@gmail.com>
2026-03-31 11:23:03 +0200

    Dodat DTO za CreateRoute

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/CreateRouteDto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TuristickiVodic.Core.DTO
+{
+    public class CreateRoutePointDto
+    {
+        [Required]
+        public short Order { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        [MaxLength(150)]
+        public string? PointName { get; set; }
+    }
+
+    public class CreateRouteDto
+    {
+        [Required, MaxLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [MaxLength(20)]
+        public string? Difficulty { get; set; }
+
+        public decimal? LengthKm { get; set; }
+
+        [Required, MinLength(2)]
+        public List<CreateRoutePointDto> RoutePoints { get; set; } = new();
+    }
+}
