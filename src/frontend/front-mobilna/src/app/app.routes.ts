6559e8e246609680d156eb50122587d0a5a32cb8
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-04-01 12:24:22 +0000

    Update app.routes.ts

--- src/frontend/front-mobilna/src/app/app.routes.ts
+++ src/frontend/front-mobilna/src/app/app.routes.ts
@@ -4,7 +4,7 @@ export const routes: Routes = [
   // { path: '', loadComponent: () => import('./feature/splash/splash').then(m => m.SplashComponent) },
   { path: '', loadComponent: () => import('./feature/login/login').then(m => m.LoginComponent) },
   { path: 'login', loadComponent: () => import('./feature/login/login').then(m => m.LoginComponent) },
-  // { path: 'register', loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent) },
+  { path: 'register', loadComponent: () => import('./feature/register/register.component').then(m => m.RegisterComponent) },
   { path: 'forgot-password', loadComponent: () => import('./feature/auth/forgot-password/forgot-password').then(m => m.ForgotPasswordComponent) },
 
   // { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
