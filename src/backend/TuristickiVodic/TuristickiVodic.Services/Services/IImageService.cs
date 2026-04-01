8f0696264cc8f24deb0b9fcf75b0364b80920d93
Marija Miljkovic <marija.miljkovic19@gmail.com>
2026-04-01 11:13:46 +0200

    Dodat interfejs IImageService

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Services/Services/IImageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TuristickiVodic.Core.DTO;
+
+namespace TuristickiVodic.Services.Services
+{
+    public interface IImageService
+    {
+        Task<IEnumerable<ImageDto>> GetAllAsync();
+        Task<ImageDto?> GetByIdAsync(int id);
+        Task<ImageDto> CreateAsync(CreateImageDto dto);
+        Task<ImageDto?> UpdateAsync(int id, UpdateImageDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
