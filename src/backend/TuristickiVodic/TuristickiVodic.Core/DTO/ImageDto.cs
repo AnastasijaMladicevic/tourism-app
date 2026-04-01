e47e02ec7543df3f539cdc719f1270136d87aae7
Marija Miljkovic <marija.miljkovic19@gmail.com>
2026-04-01 11:13:10 +0200

    Dodat ImageDto

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Core/DTO/ImageDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuristickiVodic.Core.DTO
+{
+    public class ImageDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string? AltText { get; set; }
+        public bool IsMain { get; set; }
+
+        public int? ObjectId { get; set; }
+        public int? ActivityId { get; set; }
+        public int? EventId { get; set; }
+        public int? DestinationId { get; set; }
+        public int? LocationId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
