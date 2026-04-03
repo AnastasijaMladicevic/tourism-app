b11e3534d676b513ccc056fe10ffdcebda99f6d5
Ivana Bacanin <26-2021@pmf.kg.ac.rs>
2026-04-03 15:37:03 +0200

    Odradjen izgled signout stranice

--- src/frontend/front-web/front/src/app/app.routes.ts
+++ src/frontend/front-web/front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Signout } from './pages/signout/signout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'signout',
+    component: Signout
+  }
+];
