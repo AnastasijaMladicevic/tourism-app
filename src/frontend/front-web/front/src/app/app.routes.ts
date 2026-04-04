8e36dfe2933841dfa8d1232c71d8e89dcbb52399
Mihailo Obradovic <comi.obradovic13@gmail.com>
2026-04-04 17:39:59 +0200

    dodat signup na frontend, nedostaju neke slike

--- src/frontend/front-web/front/src/app/app.routes.ts
+++ src/frontend/front-web/front/src/app/app.routes.ts
@@ -1,7 +1,23 @@
 import { Routes } from '@angular/router';
 import { Signout } from './pages/signout/signout';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
+  /*{
+      path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },*/
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./pages/signup/signup.component').then((m) => m.SignupComponent),
+  },
   {
     path: 'signout',
     component: Signout
