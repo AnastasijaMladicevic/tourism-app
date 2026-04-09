f8fe4ea3aa84d7537d0a4a0e3f633e60d04f45f0
andrija.jemovic <63-2023@pmg.kg.ac.rs>
2026-04-09 15:25:32 +0200

    Prepravka

--- src/backend/TuristickiVodic/TuristickiVodic.API/Program.cs
+++ src/backend/TuristickiVodic/TuristickiVodic.API/Program.cs
@@ -122,18 +122,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
     {
-        policy.SetIsOriginAllowed(origin =>
-              {
-                  if (!Uri.TryCreate(origin, UriKind.Absolute, out var uri))
-                  {
-                      return false;
-                  }
-
-                  return uri.Host is "localhost" or "127.0.0.1";
-              })
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+        policy.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
     });
 });
 
--- src/backend/TuristickiVodic/TuristickiVodic.Services/Services/DestinationService.cs
+++ src/backend/TuristickiVodic/TuristickiVodic.Services/Services/DestinationService.cs
@@ -23,6 +23,7 @@ namespace TuristickiVodic.Services
             var destinations = await _context.Destinations
                 .Include(d => d.DestinationType)
                 .Include(d => d.Images)
+                .Where(d => d.Images.Any(i => i.IsMain))
                 .OrderBy(d => d.Id)
                 .ToListAsync();
 
