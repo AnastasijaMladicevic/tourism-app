1dcd8850479a26787716a6ac43371cc93546f769
Andrija Jemovic <63-2023@pmf.kg.ac.rs>
2026-04-18 18:52:55 +0200

    Zavrsena about strana u profile delu

--- src/frontend/front-mobilna/src/app/feature/about/about.component.ts
+++ src/frontend/front-mobilna/src/app/feature/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
