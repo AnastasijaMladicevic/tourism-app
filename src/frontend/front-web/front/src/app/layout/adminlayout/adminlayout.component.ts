72488eb826d3abd54c11165693a8d0758e08c430
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-05-15 16:05:30 +0200

    obavestenja za admin app

--- src/frontend/front-web/front/src/app/layout/adminlayout/adminlayout.component.ts
+++ src/frontend/front-web/front/src/app/layout/adminlayout/adminlayout.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { NotificationBellComponent } from '../../shared/components/notification-bell/notification-bell.component';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, NotificationBellComponent],
   templateUrl: './adminlayout.component.html',
   styleUrls: ['./adminlayout.component.css']
 })
