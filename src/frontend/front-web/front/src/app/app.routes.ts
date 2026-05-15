39e19b19031ac480fcab5b0f7daed68dfe9467cd
Ivana Bacanin <26-2021@pmf.kg.ac.rs>
2026-05-15 14:09:00 +0200

    putanja

--- src/frontend/front-web/front/src/app/app.routes.ts
+++ src/frontend/front-web/front/src/app/app.routes.ts
@@ -272,6 +272,11 @@ export const routes: Routes = [
         path: 'map',
         loadComponent: () =>
           import('./pages/manager/map/map.component').then(m => m.ManagerMapComponent)
+      },
+      {
+        path: 'reports',
+        loadComponent: () =>
+          import('./pages/manager/reports/manager-reports.component').then(m => m.ManagerReportsComponent)
       }
     ]
   },
