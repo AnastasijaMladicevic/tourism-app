544ddfe64e61c8da2eb23d7210c74a1c62da9a74
Anastasija Mladicevic <67-2023@pmf.kg.ac.rs>
2026-06-11 00:01:57 +0200

    dodate boundary kolone na destination i region + migracija

---
+++ src/backend/TuristickiVodic/TuristickiVodic.Infrastructure/Migrations/20260610220024_AddDestinationAndRegionBoundary.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace TuristickiVodic.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDestinationAndRegionBoundary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Geometry>(
+                name: "Boundary",
+                table: "Regions",
+                type: "geometry",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Geometry>(
+                name: "Boundary",
+                table: "Destinations",
+                type: "geometry",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Regions_Boundary",
+                table: "Regions",
+                column: "Boundary")
+                .Annotation("Npgsql:IndexMethod", "GIST");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Destinations_Boundary",
+                table: "Destinations",
+                column: "Boundary")
+                .Annotation("Npgsql:IndexMethod", "GIST");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Regions_Boundary",
+                table: "Regions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Destinations_Boundary",
+                table: "Destinations");
+
+            migrationBuilder.DropColumn(
+                name: "Boundary",
+                table: "Regions");
+
+            migrationBuilder.DropColumn(
+                name: "Boundary",
+                table: "Destinations");
+        }
+    }
+}
